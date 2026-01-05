47ac2fe781f6dd2a2fe7e571e639b820a84e1e27
julio moraes <jcmcf2012@gmail.com>
2026-01-05 14:06:53 -0300

    infra: first setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/components/custom/login-form";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <LoginForm></LoginForm>
+    </main>
+  );
+}
